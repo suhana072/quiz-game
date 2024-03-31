9944d71c312cde78fd72d6f530e4f015dce2f8fc
suhana072 <154497593+suhana072@users.noreply.github.com>
2024-03-31 13:43:40 +0530

    Add files via upload

---
+++ quiz game/script.js
@@ -0,0 +1,144 @@
+const quizData = [{
+    question: "Which of the following is a client site language?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+},
+{
+    question: "Who is the father of computers?",
+    a: "james gosling",
+    b: "charles babbage",
+    c: "dennis ritchie",
+    d: "bjarne stroustrup",
+    correct: "b",
+},
+{
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "a",
+},
+{
+    question: "Which of the following computer language is written in binary codes only?",
+    a: "pascal",
+    b: "machine language",
+    c: "C",
+    d: "C#",
+    correct: "b",
+},
+{
+    question: "Which of the following is not a characteristic of a computer?",
+    a: "versatility",
+    b: "accuracy",
+    c: "diligence",
+    d: "I.Q.",
+    correct: "d",
+},
+{
+    question: "Which of the following is the smallest unit of data in a computer?",
+    a: "bit",
+    b: "kb",
+    c: "nibble",
+    d: "byte",
+    correct: "a",
+},
+{
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correct: "b",
+},
+{
+    question: "Which of the following is not a type of computer code?",
+    a: "EDIC",
+    b: "ASCII",
+    c: "BCD",
+    d: "EBCDIC",
+    correct: "a",
+},
+{
+    question: "Which of the following is designed to control the operations of a computer?",
+    a: "user",
+    b: "Application software",
+    c: "System software",
+    d: "Utility software",
+    correct: "c",
+},
+{
+    question: "What does CSS stands for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "b",
+}
+];
+let index = 0;
+let correct = 0,
+incorrect = 0,
+total = quizData.length;
+let questionBox = document.getElementById("questionBox");
+let allInputs = document.querySelectorAll("input[type='radio']")
+const loadQuestion = () => {
+if (total === index) {
+    return quizEnd()
+}
+reset()
+const data = quizData[index]
+questionBox.innerHTML = `${index + 1}) ${data.question}`
+allInputs[0].nextElementSibling.innerText = data.a
+allInputs[1].nextElementSibling.innerText = data.b
+allInputs[2].nextElementSibling.innerText = data.c
+allInputs[3].nextElementSibling.innerText = data.d
+}
+
+document.querySelector("#submit").addEventListener(
+"click",
+function() {
+    const data = quizData[index]
+    const ans = getAnswer()
+    if (ans === data.correct) {
+        correct++;
+    } else {
+        incorrect++;
+    }
+    index++;
+    loadQuestion()
+}
+)
+
+const getAnswer = () => {
+let ans;
+allInputs.forEach(
+    (inputEl) => {
+        if (inputEl.checked) {
+            ans = inputEl.value;
+        }
+    }
+)
+return ans;
+}
+
+const reset = () => {
+allInputs.forEach(
+    (inputEl) => {
+        inputEl.checked = false;
+    }
+)
+}
+
+const quizEnd = () => {
+// console.log(document.getElementsByClassName("container"));
+document.getElementsByClassName("container")[0].innerHTML = `
+    <div class="col">
+        <h3 class="w-100"> Hii, you've scored ${correct} / ${total} </h3>
+    </div>
+`
+}
+loadQuestion(index);
